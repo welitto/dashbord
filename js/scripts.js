122ee0beff155cad4ee7fd426c48bd08ad922960
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-12-23 14:42:39 -0300

    melhorando sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,48 @@
-var toggle = document.getElementsByClassName("sidebar-toggle");
-var sidebar = document.getElementsByClassName("sidebar-main");
-var contentwrapper = document.getElementsByClassName("content-wrapper");
-var sidebarmenu = document.getElementsByClassName("sidebar-menu");
+// var toggle = document.getElementsByClassName("sidebar-toggle");
+// var sidebar = document.getElementsByClassName("sidebar-main");
+// var contentwrapper = document.getElementsByClassName("content-wrapper");
+// var sidebarmenu = document.getElementsByClassName("sidebar-menu");
 
-toggle[0].addEventListener("click", function () {
-    if(sidebar[0].style.width === "" || sidebar[0].style.width === "0px"){
+// toggle[0].addEventListener("click", function () {
+//     if(sidebar[0].style.width === "" || sidebar[0].style.width === "0px"){
 
-        sidebar[0].style.width = "230px";
-        contentwrapper[0].style.marginLeft = "230px";
-        sidebarmenu[0].style.display = "block";
-    }
-    else {
-        sidebar[0].style.width = "0px";
-        contentwrapper[0].style.marginLeft = "0px";
-        sidebarmenu[0].style.display = "none";
-    }
-});
+//         sidebar[0].style.width = "230px";
+//         contentwrapper[0].style.marginLeft = "230px";
+//         sidebarmenu[0].style.display = "block";
+//     }
+//     else {
+//         sidebar[0].style.width = "0px";
+//         contentwrapper[0].style.marginLeft = "0px";
+//         sidebarmenu[0].style.display = "none";
+//     }
+// });
 
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
 
-    this.classList.toggle("active");
+//     this.classList.toggle("active");
 
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+
+
+var accbtn = document.getElementsByClassName("accordion-btn");
+var a;
+
+for(a = 0; a < accbtn.length; a++) {
+  accbtn[a].addEventListener("click", function() {
+    this.lastElementChild.classList.toggle('rotate');
+    this.nextElementSibling.classList.toggle('show');
   });
 }
