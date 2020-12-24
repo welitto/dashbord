fc583f33ac8cc68c34aa1af0b32663f2d73c786c
Wellington Junior <wellington@MacBook-Pro-de-Wellington.local>
2020-12-24 14:02:57 -0300

    organizacao

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,36 @@ var toggle = document.getElementsByClassName("sidebar-toggle");
 var sidebar = document.getElementsByClassName("sidebar-main");
 var contentwrapper = document.getElementsByClassName("content-wrapper");
 var footer = document.getElementsByClassName("footer-main");
+var accbtn = document.getElementsByClassName("accordion-btn");
 
+//pdvFullScreen();
+
+//botao de menu, no elemento header
 toggle[0].addEventListener("click", function () {
-    sidebar[0].classList.toggle("sidebar-margin-left-0");
-    contentwrapper[0].classList.toggle("content-wrapper-width-calc");
-    footer[0].classList.toggle("footer-main-none");
+
+    //verifica se esta a tela está com medida de 800px
+    if(window.matchMedia("(max-width: 800px)").matches) {
+      sidebar[0].classList.toggle("sidebar-responsive");
+      contentwrapper[0].classList.toggle("content-wrapper-responsive");
+      footer[0].classList.toggle("footer-main-none");
+    }
+    else {
+      sidebar[0].classList.toggle("pdv-form-full");
+      contentwrapper[0].classList.toggle("pdv-content-full");
+    }
+    
 });
 
-
-
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-
-//     this.classList.toggle("active");
-
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-
-var accbtn = document.getElementsByClassName("accordion-btn");
 var a;
-
 for(a = 0; a < accbtn.length; a++) {
   accbtn[a].addEventListener("click", function() {
     this.lastElementChild.classList.toggle('rotate');
     this.nextElementSibling.classList.toggle('show');
   });
+}
+
+function pdvFullScreen()
+{
+    sidebar[0].classList.toggle("pdv-form-full");
+    contentwrapper[0].classList.toggle("pdv-content-full");
 }
